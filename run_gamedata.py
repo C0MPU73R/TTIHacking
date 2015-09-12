ca5aefde6e304a98a5a0c570e356131cb7e77931
Wilee999 <Wilee999@users.noreply.github.com>
2015-09-12 14:16:32 -0700

    Create run_gamedata.py

---
+++ run_gamedata.py
@@ -0,0 +1,4 @@
+#try importing gamedata.pyd XD no infintiecipher leak yet..
+#must have infinitecipher removed from deploy.py first before u can deploy a gamedata XD
+from GameData import *
+#todo clientstart XD
