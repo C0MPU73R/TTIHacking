9cf7ef5f8761e447551aa7162370f1ad66b5fa65
Wilee999 <Wilee999@users.noreply.github.com>
2015-09-12 14:44:32 -0700

    Update run_gamedata.py

--- run_gamedata.py
+++ run_gamedata.py
@@ -1,6 +1,6 @@
 #try importing gamedata.pyd XD no infintiecipher leak yet..
 #must have infinitecipher removed from deploy.py first before u can deploy a gamedata XD
-from gamedatac.GameModules import *
+from gamedata.GameData import *
 #Test for ClientStart
 #todo figure out how to properly import data from a pyd (must be decrypted data, dont import a raw module, import the decrypted data located in the file)
 5252352453()
