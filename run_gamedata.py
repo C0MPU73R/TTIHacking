96592939ee41de3c166921393ea3bd5cd3b447a2
ponyboy837 <ponyboy837@gmail.com>
2015-09-12 17:18:07 -0400

    Update run_gamedata.py
    Added a Test for Client Start

--- run_gamedata.py
+++ run_gamedata.py
@@ -1,4 +1,7 @@
 #try importing gamedata.pyd XD no infintiecipher leak yet..
 #must have infinitecipher removed from deploy.py first before u can deploy a gamedata XD
 from GameData import *
+#Test for ClientStart
+from GameData.toontown.toonbase import ClientStart
+
 #todo clientstart XD
